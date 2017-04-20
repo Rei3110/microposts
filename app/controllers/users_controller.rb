4cb94ae1e3df730927fdee3c78d23d3d6368fafb
rei.saito <rei.s3110@gmail.com>
2017-04-20 08:06:57 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+
   def index
     @users = User.all.page(params[:page])
   end
 
   def show
-     @user = User.find(params[:id])
+    @user = User.find(params[:id])
     @microposts = @user.microposts.order('created_at DESC').page(params[:page])
     counts @user
   end
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-def create
+  def create
     @user = User.new(user_params)
 
     if @user.save
@@ -26,11 +26,22 @@ def create
       render :new
     end
   end
+
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
   
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-  
 end
